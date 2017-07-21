596535ff715a702a896a1f6a7dc88ea5f20fd1d8
Mpdreamz <mpdreamz@gmail.com>
2017-07-21 17:28:46 +0200

    moved to a module system, the default controller/views/models separation gets to me more than it maybe should, refactored search code a bit

--- src/NuSearch.Web/Program.cs
+++ src/NuSearch.Web/Program.cs
@@ -3,20 +3,20 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace NuSearch.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
 				.UseIISIntegration()
 				.UseSetting("detailedErrors", "true")
 				.CaptureStartupErrors(true)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .Build();
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseStartup<Startup>()
+				.Build();
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 }
--- src/NuSearch.Web/Startup.cs
+++ src/NuSearch.Web/Startup.cs
@@ -1,48 +1,53 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nest;
 using NuSearch.Domain;
+using NuSearch.Web.Plumbing;
 
 namespace NuSearch.Web
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
 
-        public IConfigurationRoot Configuration { get; }
+		public IConfigurationRoot Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services
+				.AddMvc(ModuleConvention.AddMvc)
+				.AddRazorOptions(ModuleRazorViewEngine.AddRazorOptions);
 
 			// register the client as a singleton
 			services.Add(ServiceDescriptor.Singleton<IElasticClient>(NuSearchConfiguration.GetClient()));
-        }
+		}
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Search}/{action=Index}");
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Search}/{action=Index}");
+			});
+		}
+	}
 }

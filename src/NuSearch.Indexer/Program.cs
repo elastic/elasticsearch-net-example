ba67c24d4b2d6f990e9714c860a6e50e6325f95d
Russ Cam <russ.cam@elastic.co>
2019-11-25 14:43:17 +1000

    Fixes for issues found in 6.x tutorial documentation. Fixes #30.

--- src/NuSearch.Indexer/Program.cs
+++ src/NuSearch.Indexer/Program.cs
@@ -152,7 +152,10 @@ namespace NuSearch.Indexer
 			Client.Alias(aliases =>
 			{
 				if (indexExists)
-					aliases.Add(a => a.Alias(NuSearchConfiguration.OldIndexAlias).Index(NuSearchConfiguration.LiveIndexAlias));
+					aliases.Add(a => a
+						.Alias(NuSearchConfiguration.OldIndexAlias)
+						.Index(Client.GetIndicesPointingToAlias(NuSearchConfiguration.LiveIndexAlias).First())
+					);
 
 				return aliases
 					.Remove(a => a.Alias(NuSearchConfiguration.LiveIndexAlias).Index("*"))
